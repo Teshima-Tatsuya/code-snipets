0fcce54cb0f8f0a3b7f736f4f9674f1fad00659a
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-11-30 21:28:31 +0900

    impl split

--- cpp/snipets.cpp
+++ cpp/snipets.cpp
@@ -19,3 +19,25 @@ int binarySearch(std::vector<int> nums, int target){
 
     return high;
 }
+
+std::vector<std::string> split(std::string str, char del) {
+    int first = 0;
+    int last = str.find_first_of(del);
+
+    std::vector<std::string> result;
+
+    while (first < str.size()) {
+        std::string subStr(str, first, last - first);
+
+        result.push_back(subStr);
+
+        first = last + 1;
+        last = str.find_first_of(del, first);
+
+        if (last == std::string::npos) {
+            last = str.size();
+        }
+    }
+
+    return result;
+}
--- cpp/snipets.h
+++ cpp/snipets.h
@@ -1,3 +1,5 @@
 #include <vector>
 
+// functions
 int binarySearch(std::vector<int>, int);
+std::vector<std::string> split(std::string, char);
